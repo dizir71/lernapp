6b3f3f1849baf8066c70e8b33fe023563eada876
dizir71 <57252011+dizir71@users.noreply.github.com>
2025-08-25 09:57:23 +0200

    Update Lernapp Stand 2025-08-25

---
+++ BWL-iOS/config.js
@@ -0,0 +1,6 @@
+// config.js – hier trägst du einmalig deinen GitHub Token ein
+const CONFIG = {
+  githubUser: "dizir71",        // dein GitHub Benutzername
+  repo: "lernapp",              // dein Repository
+  token: "ghp_TEST"  // hier den GitHub-PAT einsetzen
+};
